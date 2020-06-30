fb7aa4532631a120b6cf8b676b84ffe32dbe98c7
muhammadusamabaig <muhammadusamabaig099@gmail.com>
2020-06-30 10:27:02 -0700

    10th commit

--- src/Navigation.js
+++ src/Navigation.js
@@ -33,7 +33,7 @@ class Navigation extends Component{
            :"32px",width:"32px"}} src="covidicon.jpg"/></a> */}
             <Link href="/">Home</Link>
            <a href="#footer" className="d-none d-md-block">Term & Conditions</a>
-           <a href="#chart" >statlllkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkssssssss</a>
+           <a href="#chart" >statll</a>
 
 
            <a href="#////" className="icon" onClick={this.myFunction}>
